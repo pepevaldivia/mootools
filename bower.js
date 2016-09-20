507c4f5c099074e025bdb8938cb47cf131fac291
José Valdivia Caballero <pepe.valdivia.caballero@outlook.com>
2016-09-20 16:52:37 -0500

    Creación del primer bower.js

---
+++ bower.js
@@ -0,0 +1,22 @@
+{
+  "name": "test",
+  "description": "Probando el bower para la gestión de dependencias",
+  "main": "",
+  "license": "MIT",
+  "homepage": "",
+  "ignore": [
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "test",
+    "tests",
+    "external"
+  ],
+  "dependencies": {
+    "jquery": "^2.2.3",
+    "mootools-core ": "^1.2.6",
+    "requirejs": "^2.3.2",
+    "font-awesome": "fontawesome#^4.6.3",
+    "bootstrap": "^3.3.7"
+  }
+}
